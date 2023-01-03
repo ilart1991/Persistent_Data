74602ab8272cb2a344e338ac96f86c5da40c85ab
Артем Ильинский <42186657+ilart1991@users.noreply.github.com>
2023-01-03 12:08:45 +0300

    complete

--- second_app/lib/presentation/pages/my_home_page.dart
+++ second_app/lib/presentation/pages/my_home_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:core';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -9,12 +14,44 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+List images = [];
+
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Image.file(images[index]),
+          );
+        },
+        itemCount: images.length,
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+            controller: controller,
+            decoration: InputDecoration(
+                hintText: "Ссылка на изображение",
+                suffix: MaterialButton(
+                  onPressed: () async {
+                    var response = await http.get(Uri.parse(controller.text));
+                    Directory directory =
+                        await getApplicationDocumentsDirectory();
+                    File file = File(path.join(
+                        directory.path, path.basename(controller.text)));
+                    await file.writeAsBytes(response.bodyBytes);
+                    images.add(file);
+                    controller.clear;
+                    setState(() {});
+                  },
+                  child: const Text("Сохранить"),
+                ))),
       ),
     );
   }
